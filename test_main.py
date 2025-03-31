969699c9459d6bba87d47088aa6469aa1b1037e1
birdsoftheworld <53488356+birdsoftheworld@users.noreply.github.com>
2025-03-31 16:44:06 -0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 from main import add
 
-assert(add(2, 3) == 5)
-assert(add(-1, 1) == 0)
-assert(add(0, 0) == 0)
+def test_add():
+  assert add(2, 3) == 5
+  assert add(-1, 1) == 0
+  assert add(0, 0) == 0
